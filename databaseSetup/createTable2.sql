38008c33f635719885c64bdbbdbba64b45bb26a2
kuldeepkumarjoshi <kuldeep.joshi@metacube.com>
2015-11-28 00:20:46 +0530

    chg

---
+++ databaseSetup/createTable2.sql
@@ -0,0 +1,48 @@
+﻿create table event (
+   id bigserial NOT NULL ,
+   title character(100) default  NULL,
+   description  character(200) default NULL,
+   fromDate     DATE default NULL,
+   toDate     DATE default NULL,
+   photoUrl   character(200) default NULL,
+   request_id BIGINT  NULL,
+   isDeleted character(1),
+   CONSTRAINT eventId PRIMARY KEY (id)
+);
+
+create table requester (
+   id bigserial NOT NULL,
+   subject CHARACTER(100) default NULL,
+   name  CHARACTER(200) default NULL,
+   email   CHARACTER(200) default NULL,
+   phone CHARACTER(200) default NULL,
+   message CHARACTER(400) default NULL,
+   isDeleted CHARACTER(1),
+   CONSTRAINT requesterId PRIMARY KEY (id)
+);
+
+create table users(
+   id bigserial NOT NULL ,
+   firstName CHARACTER(100) default NULL,
+   lastName CHARACTER(100) default  NULL,
+   email CHARACTER(100) default NULL,
+   role  CHARACTER(50) default NULL,
+   dob     DATE default NULL,
+   photoUrl   CHARACTER(200) default NULL,
+   request_id BIGINT  NULL,
+   isDeleted CHAR(1),
+   CONSTRAINT usersId  PRIMARY KEY (id)
+);
+
+create table eventUser (
+   id bigserial NOT NULL ,
+   user_id BIGINT  NULL,
+   event_id BIGINT  NULL,
+   userName CHARACTER(100) default NULL,
+   eventTitle CHARACTER(100) default NULL,
+   role CHARACTER(50) default NULL,
+   isDeleted CHAR(1),
+   CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users(id),
+   CONSTRAINT event_id FOREIGN KEY (event_id) REFERENCES event(id),
+   CONSTRAINT userEventId PRIMARY KEY (id)
+);
--- databaseSetup/insertIntoTable.sql
+++ databaseSetup/insertIntoTable.sql
@@ -1,17 +1,14 @@
-insert into event(title,description,fromDate,toDate,photoUrl,request_id,isDeleted) values('raman birthDay','birsth day party','2016-01-13','2016-01-15','/kuldeep.jpg',1,'N'); 
+﻿insert into event(title,description,fromDate,toDate,photoUrl,request_id,isDeleted) values('raman birthDay','birsth day party','2016-01-13','2016-01-15','/kuldeep.jpg',1,'N'); 
 insert into event(title,description,fromDate,toDate,photoUrl,request_id,isDeleted) values('brothers marriage','marriage party','2016-02-13','2016-02-25','/marriage.jpg',2,'N'); 
 
 insert into requester(subject,name,email,phone,message,isDeleted) values('raman birth day','raghu','kkuldeepjoshi5@gmail.com','9413640466','hi please arrange this party','N'); 
 insert into requester(subject,name,email,phone,message,isDeleted) values('brothers marriage','test','testMail@gmail.com','9414617081','hi contact at once as we decssed early','N'); 
 
-insert into user(firstName,lastName,email,role,dob,photoUrl,request_id,isDeleted) values('kuldeep','joshi','kkuldeepjoshi5@gmail','ADMIN','1992-01-19','/kuldeepjoshi.jpg',null,'N'); 
-insert into user(firstName,lastName,email,role,dob,photoUrl,request_id,isDeleted) values('raghu','birla','raghu.birla@gmail','CUSTOMER','1993-02-10','/raghu.jpg',1,'N'); 
-insert into user(firstName,lastName,email,role,dob,photoUrl,request_id,isDeleted) values('salagram','sharma','sharma.salagram1@gmail','MANAGER','1990-06-07','/salag.jpg',null,'N');
+insert into users(firstName,lastName,email,role,dob,photoUrl,request_id,isDeleted) values('kuldeep','joshi','kkuldeepjoshi5@gmail','ADMIN','1992-01-19','/kuldeepjoshi.jpg',null,'N'); 
+insert into users(firstName,lastName,email,role,dob,photoUrl,request_id,isDeleted) values('raghu','birla','raghu.birla@gmail','CUSTOMER','1993-02-10','/raghu.jpg',1,'N'); 
+insert into users(firstName,lastName,email,role,dob,photoUrl,request_id,isDeleted) values('salagram','sharma','sharma.salagram1@gmail','MANAGER','1990-06-07','/salag.jpg',null,'N');
 
 insert into eventUser(user_id,event_id,userName,eventTitle,role,isDeleted) values(3,1,'salagram sharma','raman birthDay','MANAGER','N'); 
 insert into eventUser(user_id,event_id,userName,eventTitle,role,isDeleted) values(2,1,'raghu birla','raman birthDay','CUSTOMER','N'); 
 insert into eventUser(user_id,event_id,userName,eventTitle,role,isDeleted) values(3,2,'salagram sharma','brothers marriage','MANAGER','N'); 
 insert into eventUser(user_id,event_id,userName,eventTitle,role,isDeleted) values(2,2,'raghu birla','brothers marriage','CUSTOMER','N'); 
-
-
-//--- delete from event where id='1'
